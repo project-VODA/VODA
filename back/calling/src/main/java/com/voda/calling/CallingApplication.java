6b00aa7181321c247070d1f32a08ab69bfc54421
Kuma93 <kyuh2002@gmail.com>
2023-07-19 10:59:47 +0900

    [Back] first Setting - 프로젝트 기본 환경설정- - 디펜던시 추가

---
+++ back/calling/src/main/java/com/voda/calling/CallingApplication.java
@@ -0,0 +1,13 @@
+package com.voda.calling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallingApplication.class, args);
+	}
+
+}
---
+++ back/calling/src/test/java/com/voda/calling/CallingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voda.calling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
