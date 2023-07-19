0c44ccead358bbb4c6a1f022c2851b158a7cef3f
Kuma93 <kyuh2002@gmail.com>
2023-07-19 15:08:52 +0900

    [back] update directory

--- back/calling/src/main/java/com/voda/calling/model/service/UserService.java
+++ back/calling/src/main/java/com/voda/calling/model/service/UserService.java
@@ -1,5 +1,31 @@
 package com.voda.calling.model.service;
 
-public interface UserService {
+import com.voda.calling.model.dto.User;
+import com.voda.calling.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
+public class UserService {
+    UserRepository userRepository;
+
+    PasswordEncoder passwordEncoder;
+
+    public User regist(String email, String name, String password) {
+        Optional<User> existed = userRepository.findByEmail(email);
+        if(existed.isPresent()){
+            throw new EmailExistedException(email);
+        }
+        // **** 해싱하는 부분 ****
+        String encodedPassword = passwordEncoder.encode(password);
+        User user = User.builder()
+                .email(email)
+                .name(name)
+                .password(encodedPassword)
+                .level(1L)
+                .build();
+        return userRepository.save(user);
+    }
 }
