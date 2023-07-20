c8a232813f39a9de13092919a7a1050a62b330af
Jiwon <kimjw3928@naver.com>
2023-07-20 09:59:29 +0900

    [back] Swagger 설정 변경& 최상단 .idea 파일 삭제
    - user class의 handicap ApiModelProperty 설명 수정
    - userController의 ApiResponse 실패 코드 api 명세서에 맞게 수정

--- back/calling/src/main/java/com/voda/calling/controller/UserController.java
+++ back/calling/src/main/java/com/voda/calling/controller/UserController.java
@@ -3,7 +3,6 @@ package com.voda.calling.controller;
 import com.voda.calling.model.dto.User;
 import com.voda.calling.model.service.UserService;
 import io.swagger.annotations.*;
-import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,7 +23,7 @@ public class UserController {
     @ApiOperation( value = "회원가입", notes = "User 객체를 이용해 회원가입을 하는 API")
     @ApiResponses({
             @ApiResponse(code=200, message="회원가입 성공"),
-//            @ApiResponse(code=400, message="회원가입 실패")
+            @ApiResponse(code=500, message="회원가입 실패 - 서버(DB)오류")
     })
     @PostMapping("/regist")
     public ResponseEntity<User> regist(@RequestBody User user) {
