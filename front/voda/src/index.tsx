ac9a4a5186e04cc6df3e1088d951c824250f3338
gobro <gobro6366@gmail.com>
2023-07-21 14:20:41 +0900

    폰트 전역 적용

--- front/voda/src/index.tsx
+++ front/voda/src/index.tsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import store from './store';
 import { Provider } from 'react-redux';
+import './styles.css';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <Provider store={store}>
     <App />
-  </Provider>
+  </Provider>,
 );
