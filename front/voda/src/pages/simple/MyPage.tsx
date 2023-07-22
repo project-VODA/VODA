ed822c0e1a1189eca089ef8d3f979a851ab5a19c
zxskmj <zxskmj@gmail.com>
2023-07-22 18:24:09 +0900

    [Front] CheckBox 컴포넌트 생성 & singup, mypage 적용

--- front/voda/src/pages/simple/MyPage.tsx
+++ front/voda/src/pages/simple/MyPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 import Title from '../../components/Title';
@@ -6,6 +6,7 @@ import Title from '../../components/Title';
 import Input from '../../components/Input';
 import SettingButton from '../../components/SettingButton';
 import DeleteButton from '../../components/DeleteButton';
+import CheckBox from '../../components/CheckBox';
 
 
 const ButtonContainer = styled.div`
@@ -16,9 +17,6 @@ const ButtonContainer = styled.div`
   text-align: center;
 `;
 
-const RedSettingButton = styled(SettingButton)`
-  background-color: red;
-`;
 
 const SimpleMyPage = () => {
 
@@ -31,7 +29,8 @@ const SimpleMyPage = () => {
   //     />
   //   );
   // }
-  
+  const [isDisabled, setIsDisabled] = useState(false); // 장애 여부 체크 상태를 state로 관리
+
   return (
     <>
       <Title title='마이페이지' />
@@ -39,8 +38,11 @@ const SimpleMyPage = () => {
       <Input placeholder="이메일" />
       <Input placeholder="이름" />
       <Input placeholder="비밀번호" />
-      ------------------------------------ 안내 설정
-      <Input placeholder="장애 여부" />
+      <CheckBox
+        label="장애 여부" // 체크박스 옆에 표시될 텍스트
+        checked={isDisabled} // 체크 여부를 state로 전달
+        onChange={(checked) => setIsDisabled(checked)} // 체크 상태가 변경될 때 state 업데이트
+      />
 
       <ButtonContainer>
         <SettingButton text='정보 수정' />
--- front/voda/src/pages/simple/Signup.tsx
+++ front/voda/src/pages/simple/Signup.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from 'react';
 
 import Title from '../../components/Title';
 
 import Input from '../../components/Input';
-// import LoginButton from '../../components/LoginButton';
 import RegisterButton from '../../components/RegisterButton';
+import CheckBox from '../../components/CheckBox';
+
 
 const SimpleSignup = () => {
+  const [isDisabled, setIsDisabled] = useState(false); // 장애 여부 체크 상태를 state로 관리
+
   return (
     <>
       <Title title='회원가입'/>
@@ -17,7 +20,12 @@ const SimpleSignup = () => {
       <Input placeholder="비밀번호 확인" />
       <Input placeholder="전화번호" />
 
-      <RegisterButton text='로그인' />
+      <CheckBox
+        label="장애 여부" // 체크박스 옆에 표시될 텍스트
+        checked={isDisabled} // 체크 여부를 state로 전달
+        onChange={(checked) => setIsDisabled(checked)} // 체크 상태가 변경될 때 state 업데이트
+      />
+
       <RegisterButton text='회원가입' />
     </>
   );
