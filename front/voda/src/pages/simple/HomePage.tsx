1cca88542226db79143d31db23833d95ab54b76a
zxskmj <zxskmj@gmail.com>
2023-07-22 17:19:38 +0900

    [Front] Homepage에 들어가는 NavButton 추가

--- front/voda/src/pages/simple/HomePage.tsx
+++ front/voda/src/pages/simple/HomePage.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Title from '../../components/Title';
+import NavButton from '../../components/NavButton';
+
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+`;
 
 const SimpleHomePage = () => {
   return (
     <>
       <Title title='Homepage' />
+      <ButtonContainer>
+        <NavButton text="서비스 소개" to="/about" />
+        <NavButton text="영상 통화" to='/video' />
+        <NavButton text="마이 페이지" to='/mypage' />
+        <NavButton text="로그인" to='/login' />
+      </ButtonContainer>
+
       {/* Add other content for the home page */}
     </>
   );
