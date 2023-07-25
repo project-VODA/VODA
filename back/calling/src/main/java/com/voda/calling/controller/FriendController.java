184eedd9e109639e6bbf54dc5a34996b20cd7e01
Kuma93 <kyuh2002@gmail.com>
2023-07-25 12:56:28 +0900

    [back] 친구 목록 / 삭제 - 친구 목록/삭제/관계조회 서비스 구현 - 친구 목록/삭제 api 구현 // 댓글 api 예외처리

--- back/calling/src/main/java/com/voda/calling/controller/FriendController.java
+++ back/calling/src/main/java/com/voda/calling/controller/FriendController.java
@@ -2,6 +2,8 @@ package com.voda.calling.controller;
 
 import com.voda.calling.exception.NoContentException;
 import com.voda.calling.model.dto.Article;
+import com.voda.calling.model.dto.Comment;
+import com.voda.calling.model.dto.Friend;
 import com.voda.calling.model.service.FriendService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -13,6 +15,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -21,6 +24,9 @@ import java.util.Optional;
 @CrossOrigin("*")
 @Slf4j
 public class FriendController {
+
+    private static final String SUCCESS = "success";
+    private static final String FAIL = "fail";
 
     @Autowired
     FriendService friendService;
@@ -65,5 +71,53 @@ public class FriendController {
             log.info("유저 검색 실패 - 서버(DB) 오류");
             return new ResponseEntity<String>(FAIL, HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @ApiOperation( value = "친구 목록", notes = "이메일에 해당되는 친구 목록을 불러오는 api")
+    @ApiResponses({
+            @ApiResponse(code=200, message="친구 목록 호출 성공"),
+            @ApiResponse(code=204, message="친구 목록 없음 - 친구가 존재하지 않는 상태"),
+            @ApiResponse(code=500, message="친구 목록 호출 실패 - 서버(DB) 오류")
+    })
+    @GetMapping("/{userEmail}")
+    public ResponseEntity<?> searchAllFriend(@PathVariable String userEmail) {
+        log.info("FriendController - searchAllFriend : 친구 목록");
+
+        try{
+            List<Friend> friendList = friendService.searchAllFriend(userEmail);
+
+            if (friendList == null || friendList.size() == 0) {
+                return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<List<Friend>>(friendList, HttpStatus.OK);
+        }catch (Exception e) {
+            return new ResponseEntity<String>(FAIL, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+    @ApiOperation( value = "친구 삭제", notes = "이메일에 해당되는 친구를 삭제하는 api")
+    @ApiResponses({
+            @ApiResponse(code=200, message="친구 삭제 성공"),
+            @ApiResponse(code=204, message="친구 삭제 실패 - 존재하지 않는 친구 관계"),
+            @ApiResponse(code=500, message="친구 삭제 실패 - 서버(DB) 오류")
+    })
+    @DeleteMapping()
+    public ResponseEntity<String> deleteFriend(String userEmail, String friendEmail) {
+        log.info("FriendController - deleteFriend : 친구 삭제");
+
+        Friend find = friendService.searchRelationship(userEmail, friendEmail);
+
+        if (find == null) {
+            return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
+        }
+        try{
+            friendService.deleteFriend(userEmail, friendEmail);
+
+            return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+        }catch (Exception e) {
+            return new ResponseEntity<String>(FAIL, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 }
