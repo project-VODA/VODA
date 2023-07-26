48b51175c98bca3e2398472606d7015b5235bbef
gobro <gobro6366@gmail.com>
2023-07-26 17:51:10 +0900

    [Front] React Version 수정 - openVidu library에 대한 이슈 때문에 - React 18.2.0 -> React 16.13.0 버전 수정 - React 버전 다운그레이드에 대한 종속성 해결 - React dependencies 수정 - App.tsx 코드 수정

--- front/voda/src/index.tsx
+++ front/voda/src/index.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
-import store, { persistor } from "./store/store";
-import { PersistGate } from "redux-persist/integration/react";
+import store from "./store/store";
+// import { PersistGate } from "redux-persist/integration/react";
 
 import './styles.css';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
-);
 
-root.render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <App />
-    </PersistGate>
-  </Provider>,
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+    <App/>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
