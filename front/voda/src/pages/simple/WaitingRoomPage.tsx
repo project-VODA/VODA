b1fc4d1c450aa1e130c0a02d15012d67729b88f9
Kuma93 <kyuh2002@gmail.com>
2023-08-02 16:53:32 +0900

    [front] sessionStorage -> redux 사용

--- front/voda/src/pages/simple/WaitingRoomPage.tsx
+++ front/voda/src/pages/simple/WaitingRoomPage.tsx
@@ -14,14 +14,6 @@ import Input from "../../components/InputText";
 import { searchUser } from "../../apis/friend";
 import { sendCalling } from "../../apis/calling";
 
-type User = {
-  userEmail: string;
-  userName: string;
-  friend: boolean;
-};
-
-type UserList = User[];
-
 const SimpleRoom = () => {
   const [isModalOpen, setModalOpen] = useState(false);
 
@@ -37,68 +29,6 @@ const SimpleRoom = () => {
   //           console.log(err);
   //         })
   // }, [isModalOpen])
-  
-  const [keyword, setKeyword] = useState('');
-  const [userList, setUserList] = useState<UserList>([]);
-
-  const navigate = useNavigate();
-  
-  const userSearchRequest = {
-    keyword: keyword,
-    userEmail: sessionStorage.getItem("userEmail"),
-  };
-
-  // 통화 연결시 전달객체
-  let callSendRequest = {
-    senderEmail : "",
-    receiverEmail : ""
-  };
-
-  useEffect(() => {
-    searchUser(userSearchRequest)
-      .then((res) => {
-        setUserList(res);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-
-  }, [keyword]);
-
-  const handleOpenModal = () => {
-    setModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setModalOpen(false);
-    setKeyword('');
-    setUserList([]);
-  };
-
-  const handleCalling = (user: User) => {
-    callSendRequest = {
-      senderEmail : sessionStorage.getItem("userEmail"),
-      receiverEmail : user.userEmail
-    }
-
-    sendCalling(callSendRequest)
-      .then((res) => {
-        console.log(res.sessionToken);
-        navigate('/video',{
-          state: {
-            sessionToken : `${res.sessionToken}`,
-            callNo : `${res.callNo}`
-          }
-        });
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  };
-
-  const handleRegistFriend = () => {
-
-  };
 
   return (
     <>
@@ -117,30 +47,6 @@ const SimpleRoom = () => {
         >
           <RedButton onClick={(e) => setModalOpen(false)} text="X" />
           <UserSearchList/>
-          <Input 
-            type="text"
-            placeholder="검색어"
-            value={keyword}
-            onChange={(e) => setKeyword(e.target.value)}
-          />
-          <RedButton onClick={handleCloseModal} text="X" />
-          <table>
-            <colgroup>
-              <col width = "40%" />
-              <col width = "40%" />
-              <col width = "20%" />
-            </colgroup>
-            <tbody>
-              {userList.length === 0 ? <tr><td colSpan={3}>검색 결과가 없습니다.</td></tr> : 
-                userList.map((user : User) => (
-                  <tr key={user.userName}>
-                    <td>{user.userName}</td>
-                    <td>{user.userEmail}</td>
-                    <td>{user.friend ? <Button onClick={() => handleCalling(user)} text="통화 걸기" /> : <Button onClick={handleRegistFriend} text="친구추가" />}</td>
-                  </tr>
-                ))}
-            </tbody>
-          </table>
         </Modal>
       </>
     </>
