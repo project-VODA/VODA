1ecf7ec42b1980b44b6858435870cebda3fa92bf
Geon <quso12358@naver.com>
2023-08-02 11:01:12 +0900

    [front] api header jwt token 추가
    - apis 폴더 script 변경
    - 기타 필요없는 부분 주석 처리

--- front/voda/src/apis/server.ts
+++ front/voda/src/apis/server.ts
@@ -1,17 +1,18 @@
 import axios from 'axios';
 import { API_URL } from "../constants/url";
-
 import store from '../store/store';
 
-const accessToken = store.getState().auth.accessToken;
+export const axiosServer = () => {
+  let accessToken = store.getState().user.accessToken;
 
-export const axiosServer = axios.create({
-  baseURL: `${API_URL}`,
-  headers: {
-    "Content-Type": 'application/json',
-    "Authorization": `Bearer: ${accessToken}`,
-  }
-});
+  return axios.create({
+    baseURL: `${API_URL}`,
+    headers: {
+      "Content-Type": 'application/json',
+      "Authorization": `Bearer ${accessToken}`,
+    }
+  })
+};
 
 /*export const axiosServer = axios.create({
   baseURL: `${API_URL}`,
