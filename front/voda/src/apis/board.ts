a533121a86453da90ac531769662b33762abac75
Geon <quso12358@naver.com>
2023-08-02 16:14:26 +0900

    [FE] Fix API

--- front/voda/src/apis/board.ts
+++ front/voda/src/apis/board.ts
@@ -2,48 +2,48 @@ import {axiosServer} from "./server";
 
 // 게시글 목록
 export const getAricles = async () => {
-  const res = await axiosServer.get<any>(`/articles/`);
+  const res = await axiosServer().get<any>(`/articles/`);
   return res.data;
 }
 
 // 게시글 상세 내용 
 export const getArticleDetail = async (articleNo: number) => {
-  const res = await axiosServer.get<any>(`/articles/${articleNo}`);
+  const res = await axiosServer().get<any>(`/articles/${articleNo}`);
   return res.data;
 }
 
 // 게시글 작성
 export const registArticle = async (article: Object) => {
-  const res = await axiosServer.post<any>(`/articles/`, article);
+  const res = await axiosServer().post<any>(`/articles/`, article);
 }
 
 // 게시글 수정
 export const updateArticle = async (article: Object) => {
-  const res = await axiosServer.put<any>(`/articles`, article);
+  const res = await axiosServer().put<any>(`/articles`, article);
 }
 
 // 게시글 삭제
 export const deleteArticle = async (articleNo: number) => {
-  const res = await axiosServer.delete<any>(`/articles/${articleNo}`);
+  const res = await axiosServer().delete<any>(`/articles/${articleNo}`);
 }
 
 // 댓글 목록
 export const getComments = async (articleNo: number) => {
-  const res = await axiosServer.get<any>(`/comments/${articleNo}`);
+  const res = await axiosServer().get<any>(`/comments/${articleNo}`);
   return res.data;
 }
 
 // 댓글 작성
 export const registComment = async (comment: Object) => {
-  const res = await axiosServer.post<any>(`/comments`);
+  const res = await axiosServer().post<any>(`/comments`);
 }
 
 // 댓글 수정
 export const updateComment = async (comment: Object) => {
-  const res = await axiosServer.put<any>(`/comments`);
+  const res = await axiosServer().put<any>(`/comments`);
 }
 
 // 댓글 삭제
 export const deleteComment = async (commentNo: number) => {
-  const res = await axiosServer.delete<any>(`/comments/${commentNo}`);
+  const res = await axiosServer().delete<any>(`/comments/${commentNo}`);
 }
--- front/voda/src/apis/email.ts
+++ front/voda/src/apis/email.ts
@@ -2,11 +2,11 @@ import {axiosServer} from "./server";
 
 // 회원가입 인증코드 이메일 발송
 export const sendAuthenticationCode = async (email: string) => {
-  const res = await axiosServer.post<any>(`/email/regist/`, email);
+  const res = await axiosServer().post<any>(`/email/regist/`, email);
   return res.data;
 }
 
 export const sendTemporaryPassword = async (email: string) => {
-  const res = await axiosServer.post<any>(`/email/pass/`, email);
+  const res = await axiosServer().post<any>(`/email/pass/`, email);
   return res.data;
 }
