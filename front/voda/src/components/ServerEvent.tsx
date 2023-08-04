46cba003dcd8dc7f62b614b53912b6a4f4b58f06
Geon <quso12358@naver.com>
2023-08-04 13:54:13 +0900

    [all] 통화 알림 기능 추가
    - backend: eventName 설정 추가
    - frontend: 통화 알림 모달 및 통화 받기 버튼 추가

--- front/voda/src/components/ServerEvent.tsx
+++ front/voda/src/components/ServerEvent.tsx
@@ -1,20 +1,55 @@
 import React, {useEffect, useState} from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../store/store';
+import { API_URL } from '../constants/url';
+import Modal from 'react-modal';
+import { useNavigate } from 'react-router-dom';
+import { updateVideoToken } from '../store/userSlice';
 
 export default function SseComponent(){
+    const [isCallModalOpen, setisCallModalOpen] = useState(false);
+		const [data, setData] = useState({senderEmail: '', token: ''});
+
+		const navigate = useNavigate();
+		const dispatch = useDispatch();
+
     const userEmail = useSelector((state:RootState) => {
         return state.user.userInfo.userEmail;
     })
     useEffect(() => {
         if(userEmail == null || userEmail == ''){
-            return
+            return;
         }
-        const eventSource = new EventSource(`http://52.78.222.238:8080/voda/subscribe/${userEmail}`);
-        eventSource.addEventListener("sse", (event) => {
-            console.log(event);
-        })
+        const eventSource = new EventSource(`${API_URL}/subscribe/${userEmail}`);
+        eventSource.addEventListener("connection", (event) => {
+            console.log("SSE 연결 완료", event);
+        });
+        eventSource.addEventListener("call", (event) => {
+            setisCallModalOpen(true);
+						setData(JSON.parse(event.data));
+						dispatch(updateVideoToken({videoToken:data.token}))
+        });
     }, [userEmail]);
 
-    return <div></div>
+		function redirectVideo(){
+			navigate('/video');
+		}
+
+		function acceptCall(){
+			redirectVideo();
+		}
+
+    return (
+    <>
+			<Modal id="callModal"
+          isOpen={isCallModalOpen} 
+          onRequestClose={(e) => setisCallModalOpen(false)}
+          ariaHideApp={false}
+        >
+				<p>{data.senderEmail}이 전화를 걸었습니다.</p>
+				{data.token}
+				<button onClick={acceptCall}>통화 받기</button>
+      </Modal>
+    </>
+		)
 }
