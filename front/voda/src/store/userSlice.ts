9bc0adf18ed11709c373490fe9ae8469e4e73b28
Kuma93 <kyuh2002@gmail.com>
2023-08-04 14:13:30 +0900

    [all] 환경 설정 페이지 & 기능 도입 - 환경 설정 페이지, 설정 변경 버튼 / 기능 - 정보 수정, 환경 설정 수정 시 로그아웃 되게 적용

--- front/voda/src/store/userSlice.ts
+++ front/voda/src/store/userSlice.ts
@@ -13,8 +13,8 @@ const userSlice = createSlice({
             role: '',
         },
         userSetting:{
-            typeNo: '',
-            screenType: '',
+            typeNo: 0,
+            screenType: 0,
         },
         isLogin: false,
     },
@@ -61,8 +61,8 @@ const userSlice = createSlice({
                 role: '',
             };
             state.userSetting = {
-                typeNo: '',
-                screenType: '',
+                typeNo: 0,
+                screenType: 0,
             };
             state.isLogin = false;
         }
@@ -77,4 +77,9 @@ export interface UserInfoType{
     userName: string,
     userHandicap: string,
     role: string,
-}
+};
+
+export interface UserSettingType{
+    typeNo: number,
+    screenType: number,
+};
