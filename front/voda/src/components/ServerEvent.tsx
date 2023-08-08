de84c52d2c56d384622e5e2635a1baef64595162
Geon <quso12358@naver.com>
2023-08-09 08:09:28 +0900

    [front] Add browser notification
    - ServerEvent.tsx : 통화 요청 브라우저 알림 추가

--- front/voda/src/components/ServerEvent.tsx
+++ front/voda/src/components/ServerEvent.tsx
@@ -28,95 +28,110 @@ const modalColor = {
 }
 
 export default function SseComponent(){
-		const navigate = useNavigate();
-    const dispatch = useDispatch();
+	const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const [content, setContent] = useState('');
-    const [callNo, setCallNo] = useState(0);
-    const [isCallModalOpen, setisCallModalOpen] = useState(false);
-    const [userEmail, callInfo]:[string, callInfoType] = useSelector((state:RootState) => {
-        return [state.user.userInfo.userEmail, state.call.callInfo];
-    })
+  const [content, setContent] = useState('');
+  const [callNo, setCallNo] = useState(0);
+  const [isCallModalOpen, setisCallModalOpen] = useState(false);
+  const [userEmail, callInfo]:[string, callInfoType] = useSelector((state:RootState) => {
+    return [state.user.userInfo.userEmail, state.call.callInfo];
+  });
+  const [alarm, setAlarm] = useState(getNotificaationPermission());
 
-    useEffect(() => {
-        if(userEmail == null || userEmail == ''){
-            return;
-        }
-        const eventSource = new EventSource(`${API_URL}/subscribe/${userEmail}`);
-        eventSource.addEventListener("connection", (event) => {
-            console.log("SSE 연결 완료", event);
-        });
-        eventSource.addEventListener("call", (event) => {
-          //카메라가 로드되는 것을 고려해서 4초 지연 후 알림
-          setTimeout(() => {
-            setisCallModalOpen(true);
-            const response = JSON.parse(event.data);
-            dispatch(updateCall({
-              sessionToken : response.token,
-              sessionId : response.sessionId,
-              callNo : response.callNo,
-            }));
-            setContent(response.content);
-            setCallNo(response.callNo);
-          }, 4000); 
-        });
-        eventSource.addEventListener("reject", (event) => {
-            setisCallModalOpen(true);
-            // 통화 거절 추가 로직
-            const response = JSON.parse(event.data);
-            setCallNo(response.callNo);
-        })
-    }, [userEmail]);
-
-		function redirectVideo(){
-			navigate('/video');
-		}
-
-		function acceptCall(){
-      receiveCalling(callNo)
-      .then((res)=>{
-        setisCallModalOpen(false);
-        redirectVideo();
-      })
-      .catch((err)=> {
-        console.log(err);        
-      });
-		}
-
-    function rejectCall() {
-      rejectCalling(callNo)
-      .then((res) => {
-        setisCallModalOpen(false);
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      })
+  useEffect(() => {
+    if(userEmail == null || userEmail == ''){
+      return;
     }
+    const eventSource = new EventSource(`${API_URL}/subscribe/${userEmail}`);
+    eventSource.addEventListener("connection", (event) => {
+      console.log("SSE 연결 완료");
+    });
+    eventSource.addEventListener("call", (event) => {
+      //카메라가 로드되는 것을 고려해서 4초 지연 후 알림
+      setTimeout(() => {
+        setisCallModalOpen(true);
+        const response = JSON.parse(event.data);
+        dispatch(updateCall({
+          sessionToken : response.token,
+          sessionId : response.sessionId,
+          callNo : response.callNo,
+        }));
+        setContent(response.content);
+        setCallNo(response.callNo);
+        if(alarm){
+          new Notification("VODA", {body: `${response.content}`});
+        }
+      }, 4000); 
+    });
+    eventSource.addEventListener("reject", (event) => {
+      setisCallModalOpen(true);
+      // 통화 거절 추가 로직
+      const response = JSON.parse(event.data);
+      setCallNo(response.callNo);
+    });
+  }, [userEmail]);
 
-    return (
-    <>
-			<Modal id="callModal"
-          isOpen={isCallModalOpen} 
-          onRequestClose={(e) => setisCallModalOpen(false)}
-          ariaHideApp={false}
-          style={modalColor}
-        >
-        <br/>
-        <br/>
-        <br/>
-				<p style={{textAlign: 'center', fontSize: 'xx-large'}}>{content}</p>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <ButtonContainer>
-          <HandleButton text='통화 받기' onClick={acceptCall} />
-          <HandleButton text='통화 거절' onClick={rejectCall} />
-        </ButtonContainer>
-      </Modal>
-    </>
-		)
+	function redirectVideo(){
+		navigate('/video');
+	}
+
+	function acceptCall(){
+    receiveCalling(callNo)
+    .then((res)=>{
+      setisCallModalOpen(false);
+      redirectVideo();
+    })
+    .catch((err)=> {
+      console.log(err);        
+    });
+	}
+
+  function rejectCall() {
+    rejectCalling(callNo)
+    .then((res) => {
+      setisCallModalOpen(false);
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
+
+  function getNotificaationPermission() {
+    if("Notification" in window){
+      Notification.requestPermission();
+      if(Notification.permission === "granted") return true;
+      else return false;
+    }else{
+      console.log("알림을 지원하지 않는 브라우저입니다.");
+      return false;
+    }
+  }
+
+  return (
+  <>
+    <Modal id="callModal"
+      isOpen={isCallModalOpen} 
+      onRequestClose={(e) => setisCallModalOpen(false)}
+      ariaHideApp={false}
+      style={modalColor}
+    >
+      <br/>
+      <br/>
+      <br/>
+      <p style={{textAlign: 'center', fontSize: 'xx-large'}}>{content}</p>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <ButtonContainer>
+        <HandleButton text='통화 받기' onClick={acceptCall} />
+        <HandleButton text='통화 거절' onClick={rejectCall} />
+      </ButtonContainer>
+    </Modal>
+  </>
+  )
 }
