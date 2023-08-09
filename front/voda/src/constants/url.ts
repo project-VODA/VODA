4e0ab3cc6f6c8a11222b6fa43195a72395c2323a
zxskmj <zxskmj@gmail.com>
2023-08-09 16:16:46 +0900

    [AI] flask 초기 세팅 - react와 axios 연결

--- front/voda/src/constants/url.ts
+++ front/voda/src/constants/url.ts
@@ -1,5 +1,6 @@
   // baseURL: "http://localhost:8080/api",
 export const SERVER_URL = "http://localhost:3000";
+export const FLASK_URL = "http://localhost:5000";
 export const API_URL = "https://i9a707.p.ssafy.io:8080/voda";
 // export const API_URL = "http://localhost:8080/voda";
 
--- model/app.py
+++ model/app.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/')
+def index():  # put application's code here
+    return 'Index alert!!'
+
+@app.route('/hello')
 def hello_world():  # put application's code here
     return 'Hello World!'
 
