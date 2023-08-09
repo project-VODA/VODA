12823da197be6e9dd7b56214a42c43fc5d073145
gobro <gobro6366@gmail.com>
2023-08-09 15:52:11 +0900

    [Front] 마이페이지 및 파비콘
      - detail mypage 생성
      - 파비콘 및 프로젝트 이름 변경

---
+++ front/voda/src/components/DetailLogo.tsx
@@ -0,0 +1,71 @@
+// components/Title.tsx
+
+import React, { useContext } from 'react';
+
+import styled from 'styled-components';
+import { ThemeContext } from '../App';
+import DetailLogo from '../assets/images/logo_black.png'
+import { useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+
+import LandingPage from '../pages/LandingPage';
+
+interface ColorProps {
+  color: string;
+  'aria-live'?: string;
+  'aria-label'?: string;
+}
+
+const TitleContainer = styled('header')<ColorProps>`
+  width: 100%;
+  height: 60px;
+  font-size: 2em;
+  color: ${({ color }) => color};
+  font-weight: bolder;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all 0.5s ease-in-out;
+  padding: 40px 0;
+`;
+
+const Image = styled.img`
+  max-width: 300px;
+  height: auto;
+  display: block;
+  margin: 0 auto;
+`
+
+interface TitleProps {
+  title?: string;
+  className?: string;
+  'aria-live'?: string;
+  'aria-label'?: string;
+  imgSrc?: string;
+  onClick?: string;
+  tabIndex?: number;
+  // to?: string;
+}
+
+
+export default function SimpleTitle({ 'aria-live':ariaLive, 'aria-label':ariaLabel, className, imgSrc, onClick }: TitleProps) {
+  const location = useLocation()
+
+  const isLandingPage = location.pathname === '/';
+  
+  const navigate = useNavigate();
+  
+    const handleImageClick = () => {
+      navigate('/home')
+    }
+  const { theme } = useContext(ThemeContext);
+
+  return (<TitleContainer  
+            className={className} 
+            aria-live={ariaLive}
+            aria-label={ariaLabel}
+            color={theme.text} 
+            onClick={handleImageClick}>
+            <Image src={DetailLogo} alt="VODA" onClick={ isLandingPage ? handleImageClick : () => {} }/>
+          </TitleContainer>);
+}
--- front/voda/src/constants/url.ts
+++ front/voda/src/constants/url.ts
@@ -1,5 +1,8 @@
   // baseURL: "http://localhost:8080/api",
 export const SERVER_URL = "http://localhost:3000";
-// export const API_URL = "https://i9a707.p.ssafy.io:8080/voda";
-export const API_URL = "http://localhost:8080/voda";
+//배포 서버용
+export const API_URL = "https://i9a707.p.ssafy.io:8080/voda";
+
+//로컬 서버용
+// export const API_URL = "http://localhost:8080/voda";
 
