3095feceaf24a30ad20c332046bcb532791cac4c
Geon <quso12358@naver.com>
2023-08-09 16:31:49 +0900

    fix error

--- front/voda/src/components/toolbar/ToolbarComponent.tsx
+++ front/voda/src/components/toolbar/ToolbarComponent.tsx
@@ -4,5 +4,14 @@ import ToolbarComponentClass from "./ToolbarComponentClass";
 export default function ToolbarComponent(props:any){
   const navigate = useNavigate();
 
-  return <ToolbarComponentClass props={props} navigate={navigate}/>
+  return <ToolbarComponentClass
+  tabIndex = {props.tabIndex}
+  sessionId={props.sessionId}
+  user={props.user}
+  camStatusChanged={props.camStatusChanged}
+  micStatusChanged={props.micStatusChanged}
+  toggleFullscreen={props.toggleFullscreen}
+  switchCamera={props.switchCamera}
+  leaveSession={props.leaveSession}  
+  navigate={navigate}/>
 }
