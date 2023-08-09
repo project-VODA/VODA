2567bab1f520574b891247fcf4b84296c7e045ed
Jiwon <kimjw3928@naver.com>
2023-08-09 16:55:59 +0900

    [ai] testPage&axios

--- front/voda/src/apis/color.ts
+++ front/voda/src/apis/color.ts
@@ -6,8 +6,8 @@ export const getHello = async () => {
   return res.data;
 }
 
-// export const getColor = async () => {
-//   const res = await axiosFlask.get<any>(`/hello`);
-//   console.log('test hello: ', res);
-//   return res.data;
-// }
+export const colorRecognition = async (capturedImage:FormData) => {
+  const res = await axiosFlask.post<any>(`/color`, capturedImage);
+  console.log('test color: ', res);
+  return res.data;
+}
--- front/voda/src/pages/detail/TestPage.tsx
+++ front/voda/src/pages/detail/TestPage.tsx
@@ -1,22 +1,79 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 import Title from '../../components/Title';
 
-import { getHello } from "../../apis/color";
+import { getHello, colorRecognition } from "../../apis/color";
 
 
 const TestPage = () => {
-  const connectFlask = () => {
-    getHello()
-      .then((res) => { console.log(res)} )
+  const [capturedImage, setCapturedImage] = useState(null);//캡쳐된 이미지 확인용
+
+  // const connectFlask = () => {
+  //   getHello()
+  //     .then((res) => { console.log(res)} )
+  //     .catch((err)=>{ console.log(err)} )
+  // }
+
+  const getColor = (capturedImage:any) => {
+    const formData = new FormData()
+    formData.append('files',capturedImage)
+
+    colorRecognition(formData)
+      .then((res)=>{console.log(res)})
       .catch((err)=>{ console.log(err)} )
+  }
+
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    startWebcam();
+  }, []);
+
+  const startWebcam = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      videoRef.current.srcObject = stream;
+    } catch (error) {
+      console.error('웹캠을 가져오는 데 실패했습니다:', error);
+    }
+  };
+
+
+  // 화면 캡처
+  function captureScreen() {
+    const video = videoRef.current;
+    const canvas = canvasRef.current;
+
+    if (video && canvas) {
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+
+      const ctx = canvas.getContext('2d');
+      ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+      // 이미지 데이터 추출
+      const capturedImage = canvas.toDataURL('image/png'); // PNG 형식으로 추출
+      // 캡처된 이미지 표시
+      setCapturedImage(capturedImage);
+      // axios 요청보내기
+      getColor(capturedImage);
+    }
   }
 
   return (
     <>
       <Title title="테스트 페이지" />
+      <video ref={videoRef} autoPlay style={{ maxWidth: '50%', height: 'auto' }}></video>
+      <button onClick={captureScreen}>화면 캡처</button>
+      <canvas ref={canvasRef} style={{ display: 'none' }}></canvas>
 
-      <button onClick={connectFlask}>버튼</button>
+      {capturedImage && (
+        <div>
+          <h2>캡처된 이미지</h2>
+          <img src={capturedImage} alt="Captured" style={{ maxWidth: '50%', height: 'auto' }} />
+        </div>
+      )}
     </>
   );
 };
