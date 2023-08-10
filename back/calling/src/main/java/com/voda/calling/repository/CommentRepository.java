a2f998a67fcd42750daa3f9a65e026732fbfb68c
Kuma93 <kyuh2002@gmail.com>
2023-08-10 16:35:40 +0900

    [all] 임시

--- back/calling/src/main/java/com/voda/calling/repository/CommentRepository.java
+++ back/calling/src/main/java/com/voda/calling/repository/CommentRepository.java
@@ -2,12 +2,17 @@ package com.voda.calling.repository;
 
 import com.voda.calling.model.dto.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    List<Comment> findAllByArticleNo(int articleNo);
+    @Query("SELECT c.commentNo, u.userName, c.commentContent, c.commentRegTime " +
+            "FROM Comment c " +
+            "JOIN User u ON c.userEmail = u.userEmail " +
+            "WHERE c.articleNo = :articleNo AND c.commentCancel = 0")
+    List<Comment> findAllIncludeUserNameByArticleNo(int articleNo);
 
     Comment findByCommentNo(int commentNo);
 
--- front/voda/src/pages/simple/board/DetailArticlePage.tsx
+++ front/voda/src/pages/simple/board/DetailArticlePage.tsx
@@ -8,6 +8,7 @@ import Title from '../../../components/Title';
 import Button from '../../../components/RegisterButton';
 import ArticleHeader from '../../../components/board/ArticleHeader';
 import ArticleContent from '../../../components/board/ArticleContent';
+import CommentList from '../../../components/board/CommentList';
 
 const StyledLink = styled(Link)`
 text-decoration: none;
@@ -50,26 +51,26 @@ const SimpleDetailArticle = () => {
     }
 
     return (
+      <>
+        <StyledLink to='' aria-label={`${detailBoardData.articleTitle} 상세 조회 페이지입니다.`}>
+          <Title title={detailBoardData.articleTitle} />
+        </StyledLink>
+          <ItemContainer>
+              {/* 게시글 header */}
+              {/* <StyledLink to='' aria-label={`${detailBoardData.articleNo}번째 글, 작성일시 ${detailBoardData.articleRegTime}`}> */}
+            <ArticleHeader articleNo={detailBoardData.articleNo} articleRegDate={detailBoardData.articleRegTime} aria-label={`${detailBoardData.articleNo}번째 글, 작성일시 ${detailBoardData.articleRegTime}`}/>
+              {/* </StyledLink> */}
+              {/* 게시글 content */}
+              {/* <StyledLink to='' aria-label={`게시글 내용, ${detailBoardData.articleTitle}`}> */}
+            <ArticleContent articleContent={detailBoardData.articleContent} aria-label={`게시글 내용, ${detailBoardData.articleContent}`}/>
+              {/* </StyledLink> */}
+            {/* <CommentContent></CommentContent> */}
+            <CommentList articleNo={articleNo} />
+          </ItemContainer>
 
-        <>
-          <StyledLink to='' aria-label={`${detailBoardData.articleTitle} 상세 조회 페이지입니다.`}>
-            <Title title={detailBoardData.articleTitle} />
-          </StyledLink>
-            <ItemContainer>
-                {/* 게시글 header */}
-                {/* <StyledLink to='' aria-label={`${detailBoardData.articleNo}번째 글, 작성일시 ${detailBoardData.articleRegTime}`}> */}
-                  <ArticleHeader articleNo={detailBoardData.articleNo} articleRegDate={detailBoardData.articleRegTime} aria-label={`${detailBoardData.articleNo}번째 글, 작성일시 ${detailBoardData.articleRegTime}`}/>
-                {/* </StyledLink> */}
-
-                {/* 게시글 content */}
-                {/* <StyledLink to='' aria-label={`게시글 내용, ${detailBoardData.articleTitle}`}> */}
-                  <ArticleContent articleContent={detailBoardData.articleContent} aria-label={`게시글 내용, ${detailBoardData.articleContent}`}/>
-                {/* </StyledLink> */}
-            </ItemContainer>
-
-            {/* <Button text='수정' onClick={ModifyOneArticle} /> */}
-            <Button text='목록' onClick={RedirectListPage} />
-        </>
+          {/* <Button text='수정' onClick={ModifyOneArticle} /> */}
+          <Button text='목록' onClick={RedirectListPage} />
+      </>
     )
 };
 
