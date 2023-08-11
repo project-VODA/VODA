a9494c094251faea00e70e141687c493d8147c48
Kuma93 <kyuh2002@gmail.com>
2023-08-11 14:11:40 +0900

    [front] 고객의소리함 댓글 목록 / 관련 버튼 추가

--- front/voda/src/constants/url.ts
+++ front/voda/src/constants/url.ts
@@ -6,7 +6,7 @@ const apiKey = 'AIzaSyC5prZmvmHmlj2gh_jqjOcFTLh1CLhOzxM';
 export const GOOGLE_API_URL = "https://texttospeech.googleapis.com/v1/text:synthesize";
 
 //배포 서버용
-export const API_URL = "https://i9a707.p.ssafy.io:8080/voda";
+// export const API_URL = "https://i9a707.p.ssafy.io:8080/voda";
 
 //로컬 서버용
-// export const API_URL = "http://localhost:8080/voda";
+export const API_URL = "http://localhost:8080/voda";
