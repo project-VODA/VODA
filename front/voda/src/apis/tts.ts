613ebf63b1d55da7c2735cf15118be1a1c924cf9
Jiwon <kimjw3928@naver.com>
2023-08-11 11:17:10 +0900

    [ai] tts에 userSetting 적용

--- front/voda/src/apis/tts.ts
+++ front/voda/src/apis/tts.ts
@@ -1,7 +1,7 @@
 import { axiosGoogle } from "./server";
 
 const apiKey = 'AIzaSyC5prZmvmHmlj2gh_jqjOcFTLh1CLhOzxM';
-export const colortts = async (requestData:object) => {
-  const res = await axiosGoogle.post<any>(`?key=${apiKey}` ,requestData)
+export const tts = async (requestData:object) => {
+  const res = await axiosGoogle.post<any>(`${apiKey}` ,requestData)
   return res.data;
 }
