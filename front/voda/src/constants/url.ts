576043a2c6575609064dfbcd2387151b7e9d44e5
sunhee3859 <sunhee3859@naver.com>
2023-08-16 11:57:22 +0900

    flask SSL error 해결

--- front/voda/src/constants/url.ts
+++ front/voda/src/constants/url.ts
@@ -3,7 +3,7 @@ export const SERVER_URL = "http://localhost:3000";
 
 //배포 서버용
 export const API_URL = "https://i9a707.p.ssafy.io:8080/voda";
-export const FLASK_URL = "https://i9a707.p.ssafy.io/flask";
+export const FLASK_URL = "https://www.vodavoda.site/flask";
 export const GOOGLE_API_URL = "https://texttospeech.googleapis.com/v1/text:synthesize";
 
 //로컬 서버용
