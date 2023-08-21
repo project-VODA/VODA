759c713862be9080958966f0942d96e7edf3869d
Gobro-s <gobro6366@gmail.com>
2023-08-22 00:22:15 +0900

    [Front] 날씨 5초마다 변경기능 삭제

--- front/voda/src/components/weather/WeatherCurrent.tsx
+++ front/voda/src/components/weather/WeatherCurrent.tsx
@@ -1,10 +1,8 @@
-import React, { useEffect, useState, useRef } from "react"
+import React, { useEffect, useState, } from "react"
 import { styled } from "styled-components"
 import weather from "../../apis/weather";
 import { BsFillBrightnessHighFill, BsFillCloudFill, BsFillCloudLightningFill, BsCloudDrizzleFill, BsFillCloudRainFill, BsFillCloudSnowFill, BsFillCloudHaze2Fill, BsEmojiAngry, BsArrowLeftShort, BsArrowRightShort } from "react-icons/bs";
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md"
-import { CSSTransition } from 'react-transition-group';
-import '../../styles/detail/Weather.css'
 
 interface WeatherOption {
   icon: React.ComponentType;
@@ -94,28 +92,6 @@ const WeatherCurrent = () => {
   const [currentCityIndex, setCurrentCityIndex] = useState(0);
   const currentCity = cities[currentCityIndex];
   const [currentCityKorean, setCurrentCityKorean] = useState(currentCity);
-  const [autoRotate, setAutoRotate] = useState(true); // 기본값 true로 설정
-  const autoRotateInterval = 5000; // 5초
-  const intervalId = useRef<NodeJS.Timer | undefined>(); // useRef로 intervalId 선언
-
-  useEffect(() => {
-    if (autoRotate) {
-      intervalId.current = setInterval(() => {
-        nextCity();
-      }, autoRotateInterval);
-    }
-
-    return () => {
-      if (intervalId.current) {
-        clearInterval(intervalId.current);
-      }
-    };
-  }, [autoRotate]);
-
-
-  const toggleAutoRotate = () => {
-    setAutoRotate(prevState => !prevState);
-  };
 
   const nextCity = () => {
     setCurrentCityIndex((prevIndex) => (prevIndex + 1) % cities.length);
@@ -185,16 +161,9 @@ const WeatherCurrent = () => {
         <div>
           <IconContainerWrapper>
             <MdKeyboardArrowLeft onClick={prevCity} size={23}/>
-            <CSSTransition
-              in={true} // 슬라이드가 나타난 상태인 경우
-              appear={true} // 컴포넌트가 처음에 마운트될 때도 애니메이션 적용
-              timeout={500} // 애니메이션 지속 시간 (밀리초)
-              classNames="slide" // 애니메이션에 사용할 클래스 이름
-            >
               <IconContainer>
                 {currentCityKorean} <WeatherIcon weather={weatherInfo.weather} size={23} /> {Math.round(weatherInfo.temp)}°C {weatherInfo.weather}
               </IconContainer>
-            </CSSTransition>
             <MdKeyboardArrowRight onClick={nextCity} size={23}/>
           </IconContainerWrapper>
         </div>
